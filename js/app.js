1dd5302f58abeca5ee98046a1e53ae6162db80c9
stephen mwangi <stephemmw@gmail.com>
2024-03-17 14:01:08 +0300

    add additional contact  number

--- js/app.js
+++ js/app.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
         const services = document.getElementById("services");
         services.scrollIntoView();
     });
+
+    changeBackground();
 });
+
+function changeBackground(){
+    window.onscroll = function() {
+    var currentScrollPos = window.scrollY;
+  
+    // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+    if (currentScrollPos > 20) {
+      // I am using 'display' instead of 'top':
+      document.getElementById("primary-nav").style.backgroundColor = "#700018";
+    } else {
+      document.getElementById("primary-nav").style.backgroundColor = "#8D021F";
+    }
+  }
+}
+
