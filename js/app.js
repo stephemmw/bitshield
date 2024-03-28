f4e18f6491ede19a1cfeb0c79f3788108c7b2fb9
stephen mwangi <stephemmw@gmail.com>
2024-03-28 15:58:59 +0300

    Add red and blue themes.

--- js/app.js
+++ js/app.js
@@ -8,20 +8,67 @@ $(document).ready(function(){
         const services = document.getElementById("services");
         services.scrollIntoView();
     });
-    changeBackground();
+
+    //Change theme
+    $("#default-theme").click(function(){
+      changeTheme('default');
+    });
+    $("#red-theme").click(function(){
+      changeTheme('red');
+    });
+    $("#blue-theme").click(function(){
+      changeTheme('blue');
+    });
+
+    changeNavBackground();
 });
 
-function changeBackground(){
+function changeNavBackground(){
     window.onscroll = function() {
     var currentScrollPos = window.scrollY;
   
     // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
     if (currentScrollPos > 20) {
-      // I am using 'display' instead of 'top':
       document.getElementById("primary-nav").style.boxShadow = "2px 2px 8px 2px #000";
     } else {
-      document.getElementById("primary-nav").style.boxShadow = "2px 2px 4px 4px #fff";
+      document.getElementById("primary-nav").style.boxShadow = "0px 0px 0px 0px transparent";
     }
+  }
+}
+
+function changeTheme(theme){
+  const $themeStyle = document.getElementById('theme');
+  const $telecomms = document.getElementById("img-telecomms");
+  const $cyber = document.getElementById("img-cyber");
+  const $data = document.getElementById("img-data");
+  const $software = document.getElementById("img-software");
+  const $training = document.getElementById("img-training");
+  
+
+  if (theme ===  'default'){
+    $themeStyle.setAttribute("href", "css/themes/default.css");
+    $telecomms.setAttribute("src", "images/icons/telecommunication.png");
+    $cyber.setAttribute("src", "images/icons/cybersecurity.png");
+    $data.setAttribute("src", "images/icons/data-management.png");
+    $software.setAttribute("src", "images/icons/custom.png");
+    $training.setAttribute("src", "images/icons/training.png");
+    
+  }
+  else if (theme === 'red'){
+    $themeStyle.setAttribute("href", "css/themes/red.css");
+    $telecomms.setAttribute("src", "images/icons/telecommunication-alt.png");
+    $cyber.setAttribute("src", "images/icons/cybersecurity-alt.png");
+    $data.setAttribute("src", "images/icons/data-management-alt.png");
+    $software.setAttribute("src", "images/icons/custom-alt.png");
+    $training.setAttribute("src", "images/icons/training-alt.png");
+  }
+  else if (theme === 'blue'){
+    $themeStyle.setAttribute("href", 'css/themes/blue.css');
+    $telecomms.setAttribute("src", "images/icons/telecommunication-alt.png");
+    $cyber.setAttribute("src", "images/icons/cybersecurity-alt.png");
+    $data.setAttribute("src", "images/icons/data-management-alt.png");
+    $software.setAttribute("src", "images/icons/custom-alt.png");
+    $training.setAttribute("src", "images/icons/training-alt.png");
   }
 }
 
